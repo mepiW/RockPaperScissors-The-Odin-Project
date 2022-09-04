eba8a560a552833b90a4fe2add86e673a84ca6b5
mepiW <mt.tsonev86@gmail.com>
2022-09-04 14:04:33 +0300

    Styling

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
 const playBtn = document.querySelector("button");
 const inputField = document.querySelector("input");
+
+const playerChoiceDisplay = document.getElementById("pc");
+const computerChoiceDisplay = document.getElementById("cc");
+const resultDisplay = document.getElementById("result");
+
 const choices = ["rock", "paper", "scissors"];
 
 let playerSelection;
@@ -7,40 +12,44 @@ let computerSelection;
 
 function getComputerChoice() {
     computerSelection =  choices[Math.floor(Math.random() * choices.length)];
-    return console.log(computerSelection);
+    return computerChoiceDisplay.innerHTML = `Computer choice: ${computerSelection}`;
 }
 playBtn.onclick = function getPlayerChoice() { 
     if (inputField.value == "") {
-        alert("You must type your selection");
+        alert("You must type your selection!");
     } else {
         playerSelection = inputField.value;
         playerSelection = playerSelection.toLowerCase(playerSelection);
         playerSelection = playerSelection.replaceAll(" ", "");
         
-        console.log(playerSelection);
+        playerChoiceDisplay.innerHTML = `Player choice: ${playerSelection}`;
         getComputerChoice(playerSelection);
         checkWinner();
-    }
-    
-}
 
-function playRound(computerChoice, playerChoice) {
+        inputField.value = "";
+    }
 }
 
 function checkWinner() {
     if (playerSelection === computerSelection) {
-        console.log("Tie!");
+        resultDisplay.innerHTML = "Result: Tie!";
     } else if (
         (playerSelection === "rock" && computerSelection == "scissors") ||
         (playerSelection === "paper" && computerSelection == "rock") ||
         (playerSelection === "scissors" && computerSelection == "paper")
     ){
-                console.log("You win!");
+        resultDisplay.innerHTML = "Result: You win!";
     } else if (
         (playerSelection === "rock" && computerSelection == "paper") ||
         (playerSelection === "paper" && computerSelection == "scissors") ||
         (playerSelection === "scissors" && computerSelection == "rock")
     ){
-        console.log("You lost");
+        resultDisplay.innerHTML = "Result: You lost!";
+    } else {
+        alert(`${playerSelection} is not part of the choices!`);
+        inputField.value = "";
+        playerChoiceDisplay.innerHTML = "Player choice: ";
+        computerChoiceDisplay.innerHTML = "Computer choice: ";
+        resultDisplay.innerHTML = "Result: "
     }
 }
